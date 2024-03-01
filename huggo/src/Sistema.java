1ac06dfc7614c168a6d16008dc0c4d6a80253502
huggoparcelly <h.parcelly@gmail.com>
2024-03-01 15:53:54 -0300

    criacao da classe main e do objeto voos

---
+++ huggo/src/Sistema.java
@@ -0,0 +1,8 @@
+public class Sistema {
+    public static void main(String[] args) {
+        
+        
+
+
+    }
+}
---
+++ huggo/src/Voos.java
@@ -0,0 +1,17 @@
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+public class Voos {
+
+  LocalDate data;
+  LocalTime horarioSaida;
+  LocalTime horarioChegada;
+  BigDecimal valor;
+  String origem;
+  String destino;
+  Integer assentosDisponiveis;
+
+
+}
+
